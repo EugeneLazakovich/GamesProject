486412d7f2c1c9e63ae73bc340e26a93741d6de3
EugeneLazakovich EugeneLazakovich <e.lazakovich@center-ecm.com>
2022-02-15 22:30:02 +0200

    first commit

---
+++ GamesHM1/Controllers/GamesController.cs
@@ -0,0 +1,77 @@
+﻿using GamesHM1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GamesHM1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GamesController : ControllerBase
+    {
+        private static List<Game> _games;
+        private readonly ILogger<GamesController> _logger;
+        static GamesController()
+        {
+            _games = new List<Game>();
+        }
+        public GamesController(ILogger<GamesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult AddGame(Game game)
+        {
+            game.Id = Guid.NewGuid();
+            _games.Add(game);
+
+            return Created(game.Id.ToString(), game);
+        }
+
+        [HttpGet]
+        public IActionResult GetAllGames()
+        {
+            return Ok(_games);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetGameById(Guid id)
+        {
+            Game game = _games.FirstOrDefault(game => game.Id == id);
+            if(game != null)
+            {
+                return Ok(game);
+            }
+            return NotFound();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateGame(Game game)
+        {
+            Game dbGame = _games.FirstOrDefault(c => c.Id == game.Id);
+            if(dbGame != null)
+            {
+                var index = _games.IndexOf(dbGame);
+                _games[index] = game;
+                return Ok(game);
+            }
+            return NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteGame(Guid id)
+        {
+            Game game = _games.FirstOrDefault(game => game.Id == id);
+            if(game != null)
+            {
+                _games.Remove(game);
+                return Ok(game);
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ GamesHM1/Models/Game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GamesHM1.Models
+{
+    public class Game
+    {
+        [Required]
+        public Guid Id { get; set; }
+        [MinLength(2)]
+        [MaxLength(30)]
+        [Required(ErrorMessage = "Game name is required")]
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public DateTime DateReleased { get; set; }
+        [RegularExpression(@"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}", ErrorMessage = "Please enter correct email")]
+        public string SupportEmail { get; set; }
+        [Range(0, 10000000, ErrorMessage = "Buys must be between 0 and 10000000")]
+        public int CountBuys { get; set; }
+    }
+}
